48c0092d5ece92df3011a93a9128376214516979
Natalia Mazurczak <natalia.mazurczak@o2.pl>
2021-06-14 09:27:04 +0200

    Create react app, add styled components and styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        background: 'rebeccapurple',
+        black: '#282c34',
+        blue: '#a0e9fd',
+        lightblue: '#caf3fe',
+        white: 'white'
+    },
+    transition: '0.3s',
+};
+
+export default theme;
